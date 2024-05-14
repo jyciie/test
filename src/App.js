fd9b6b74f343f549ba3466961c163f6fc3e943d2
Joyce Avila <avilamaryjoycs@gmail.com>
2024-05-14 20:45:09 +0800

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,93 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import FormInputField from './components/FormInputField';
-import {
-  getUser,
-  getUserImages,
-  getUserImageByID,
-  getUserByIDUser,
-  createUser,
-  updateUser,
-  deleteUser,
-} from './api/userApi';
+import { getUser, createUser } from './api/userApi';
 
-// const myTable = [];
-
-const fileDataToUri = (file) =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      resolve(event.target.result);
-    };
-    reader.readAsDataURL(file);
-  });
 function App() {
-  // const [FName, setFName] = useState();
-  // const [MName, setMName] = useState();
-  // const [LName, setLName] = useState();
-  // const [BDate, setBDate] = useState();
-  // const [Address, setAddress] = useState();
-  // const [ContactNumber, setContactNumber] = useState();
-  const [myTable, setMyTable] = useState([]); //manage state application
-  const [myImg, setmyImg] = useState();
+  const [myTable, setMyTable] = useState([]);
   useEffect(() => {
     getUser().then((result) => {
       setMyTable(result.data);
       console.log(result);
     });
-  }, []); //rerender app
+  }, []);
 
   const {
     register: myRegister,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
 
-  // const olduser = {
-  //   lastName: 'kelvin',
-  //   fname: 'vegilla',
-  // };
-
-  // const newuser = { ...olduser, address: 'Home' };
-
-  // console.log(olduser);
-  // console.log(newuser);
-
-  // state name , state update name
   function saveDetails(formData) {
     console.log(formData);
-    formData.myImg = myImg;
-    setMyTable([
-      ...myTable,
-      // {
-      //   LName,
-      //   FName,
-      //   MName,
-      //   BDate,
-      //   Address,
-      //   ContactNumber,
-      //   myImg,
-      // },
-      formData,
-    ]);
     createUser(formData);
-
-    //myTable.push();
-    // console.log(myTable);
-  }
-
-  function handleOnchange(file) {
-    if (!file) {
-      setmyImg('');
-      return;
-    }
-
-    fileDataToUri(file).then((dataUri) => {
-      setmyImg(dataUri);
-    });
   }
 
   return (
@@ -151,80 +85,15 @@ function App() {
             errorclass='text-redx'
           />
 
-          {/* <div className='text-left my-1'>
-            <label>Last Name</label>
-            <input
-              type='text'
-              className='rounded-xs border uppercase w-full my-1'
-              // onChange={(e) => setLName(e.target.value)}
-              {...myRegister('LName', { required: true, maxLength: 30 })}
-            />
-            {errors.LName && errors.LName.type === 'required' && (
-              <span className='text-redx'>This is required</span>
-            )}
-            {errors.LName && errors.LName.type === 'maxLength' && (
-              <span className='text-redx'>Max length exceeded</span>
-            )}
-          </div>
-          <div className='text-left my-1'>
-            <label>First Name</label>
-            <input
-              type='text'
-              className='rounded-xs border uppercase w-full my-1'
-              //onChange={(e) => setFName(e.target.value)}
-              {...myRegister('FName')}
-            />
-          </div>
-
-          <div className='text-left my-1'>
-            <label>Middle Name</label>
-            <input
-              type='text'
-              className='rounded-xs border uppercase w-full my-1'
-              //onChange={(e) => setMName(e.target.value)}
-              {...myRegister('MName')}
-            />
-          </div>
-
-          <div className='text-left my-1'>
-            <label>Birth Date</label>
-            <input
-              type='date'
-              className='rounded-xs border uppercase w-full my-1'
-              //onChange={(e) => setBDate(e.target.value)}
-              {...myRegister('BDate')}
-            />
-          </div>
-
-          <div className='text-left my-1'>
-            <label>Contact Number</label>
-            <input
-              type='text'
-              className='rounded-xs border uppercase w-full my-1'
-              //onChange={(e) => setContactNumber(e.target.value)}
-              {...myRegister('ContactNumber')}
-            />
-          </div>
-
-          <div className='text-left my-1'>
-            <label>Address</label>
-            <input
-              type='text'
-              className='rounded-xs border uppercase w-full my-1'
-              //onChange={(e) => setAddress(e.target.value)}
-              {...myRegister('txtAddress')}
-            />
-          </div> */}
-
-          <div className='text-left my-1'>
-            <label>Image</label>
-            <input
-              type='file'
-              className='w-full my-1'
-              //{...myRegister('myImg')}
-              onChange={(e) => handleOnchange(e.target.files[0] || null)}
-            />
-          </div>
+          <FormInputField
+            register={myRegister}
+            name={'ImageBlob'}
+            fieldlabel={'Profile Picture'}
+            validation={{ required: true }}
+            errors={errors}
+            errorclass='text-redx'
+            fieldtype='file'
+          />
 
           <button
             className='rounded-md border bg-zinc-900 text-white h-[35px] px-2'
@@ -242,6 +111,7 @@ function App() {
                 <img
                   className='border h-[150px] w-full rounded-full'
                   src={element.myImg}
+                  alt='Profile'
                 ></img>
                 <div>
                   <div className='text-left my-1'>
@@ -265,37 +135,6 @@ function App() {
             );
           })}
         </div>
-
-        {/* <table className="table table-fixed border-separate border border-slate-400">
-          <thead>
-            <tr>
-              <th className="border border-slate-300">#</th>
-              <th className="border border-slate-300">Firs Name</th>
-              <th className="border border-slate-300">Middle Name</th>
-              <th className="border border-slate-300">Last Name</th>
-              <th className="border border-slate-300">Birth Date</th>
-              <th className="border border-slate-300">Contact Number</th>
-              <th className="border border-slate-300">Address</th>
-            </tr>
-          </thead>
-          <tbody>
-            {myTable.map((element, indx) => {
-              return (
-                <tr key={indx}>
-                  <td className="border border-slate-300">{indx + 1}</td>
-                  <td className="border border-slate-300">{element.FName}</td>
-                  <td className="border border-slate-300">{element.MName}</td>
-                  <td className="border border-slate-300">{element.LName}</td>
-                  <td className="border border-slate-300">{element.BDate}</td>
-                  <td className="border border-slate-300">
-                    {element.ContactNumber}
-                  </td>
-                  <td className="border border-slate-300">{element.Address}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table> */}
       </div>
     </div>
   );
